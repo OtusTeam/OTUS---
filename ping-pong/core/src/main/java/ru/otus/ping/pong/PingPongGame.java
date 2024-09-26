0d03f866ad9210ee1b8939e5a5586c6e58f00cb6
Roman Voronovskii <121174759+RomanVoronovskiy@users.noreply.github.com>
2024-09-26 19:00:56 +0300

    add ping-pong project

---
+++ ping-pong/core/src/main/java/ru/otus/ping/pong/PingPongGame.java
@@ -0,0 +1,15 @@
+package ru.otus.ping.pong;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class PingPongGame extends ApplicationAdapter {
+
+}
